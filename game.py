0310477153ee54ac44aff325f1239a4c749850f0
Jonathan Mares <jm2242@cornell.edu>
2017-02-18 17:02:43 -0500

    basic string representation of board, set 1 row pawns, some functions to check valid coordinate and do some basic conversions

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
+from piece import Piece, PieceType
+import numpy as np
 
 class Game:
     def __init__(self):
 
         # initialize game state
-        board = Board()
-
-        self.winner = self.play()
-
+        self.board = Board()
+        print(self.board)
 
     # plays the game
     def play(self):
@@ -14,17 +14,71 @@ class Game:
         # main game loop
         while True:
 
-
-
-
-
-
+            # check if there is a winner
+            if self.winner():
+                print("{0} is the Winner!".format(self.winner))
+                break
+        return
 
 class Board:
     def __init__(self):
-        self.board = 8*[8*[None]]
+        self.board = np.array([8*[None] for _ in range(8)])
+
+        # set pieces according to standard rules
+        self.set_pieces()
 
 
+    def set_pieces(self):
+
+        # set pawns
+        for i in range(1,9):
+            pawn = Piece(PieceType.PAWN, 1)
+            self.place_piece(pawn, 7, self.col_to_letter(i))
+
+    # place a piece in the selected square
+    def place_piece(self, piece, row, col):
+
+        self.check_valid_coord(row, col)
+
+        # invert row number
+        row_set = 8 - row
+
+        # convert col to integer
+        col_set = ord(col) - ord('a')
+
+        # make sure square is empty
+        if self.board[row_set][col_set] is not None:
+            raise Exception("{0},{1} is not empty".format(row, col))
+
+        self.board[row_set][col_set] = piece
+
+
+    def col_to_letter(self, col):
+        return chr(ord('a') + col - 1)
+
+    def check_valid_coord(self, row, col):
+        # validate row, col
+        col.lower()
+        rowInvalid = (row < 1 or row > 8)
+        colInvalid = (col < 'a' or col > 'h')
+        if rowInvalid or colInvalid:
+            raise Exception("invalid row/col combination")
+
+
+    def __repr__(self):
+        chessBoard = []
+        for row in self.board:
+            rowAsList = ['  ' + piece.__str__() + ' ' if piece is not None else '  *  ' for piece in row]
+            chessBoard.append(''.join(rowAsList) + '\n')
+        return str(''.join(chessBoard))
+
+    def __str__(self):
+        chessBoard = []
+        for idr, row in enumerate(self.board):
+            rowAsList = [str(8 - idr) + '|' ] + ['  ' + piece.__str__() + ' ' if piece is not None else '  *  ' for piece in row]
+            chessBoard.append(''.join(rowAsList) + '\n')
+        chessBoard.append(''.join([' ' + '   a ' + '   b ' + '   c ' + '   d ' + '   e '+ '   f ' + '   g '+ '   h ']))
+        return str(''.join(chessBoard))
 
 
 
@@ -32,5 +86,10 @@ class Player:
     def __init__(self, name):
         self.name = name
 
-# Pawn, Rook, Knight, Bishop, Queen, King
-class Piece:
+
+def main():
+    game = Game()
+    #game.play()
+
+if __name__ == "__main__":
+    main()
--- piece.py
+++ piece.py
@@ -1,9 +1,21 @@
 from enum import Enum
 
-class Piece(Enum):
-    PAWN = 1
-    KNIGHT = 3
-    BISHOP = 3
-    ROOK = 5
-    QUEEN = 8
-    KING = 10
+class PieceType(Enum):
+    PAWN = "P"
+    KNIGHT = "K"
+    BISHOP = "B"
+    ROOK = "R"
+    QUEEN = "Q"
+    KING = "K"
+
+
+class Piece:
+    def __init__(self, pieceType, player):
+        self.piece_type = pieceType
+        self.player = player
+
+    def __str__(self):
+        return (self.piece_type.name[0] + str(self.player))
+
+    def __repr__(self):
+        return (self.piece_type.name[0] + str(self.player))
